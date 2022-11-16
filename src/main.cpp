dd900b54d59a233a9f5e56539334945534a46a16
LockRR <hemistergt@gmail.com>
2022-11-16 22:23:54 +0700

    last

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,71 @@
 #include <Arduino.h>
 
+const int LEDBuiltIn = PC13;
+const int potPin = PA2;
+const int LEDEksternalPin = PA1;
+const int key = PA0;
+float ledFade = 0;
+unsigned long lastFadeTime;
+const long fadeTime = 20;
+float dimSpeed = 10;
+int buttonState;
+bool fadeup;
+int potValue;
+double vPot;
 void setup()
 {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(PC13, OUTPUT);
-  ;
-  Serial.begin(9600);
+  pinMode(LEDBuiltIn, OUTPUT);
+  // Untuk potensio
+  pinMode(potPin, INPUT);
+  // led eksternal
+  pinMode(LEDEksternalPin, OUTPUT);
+  pinMode(key, INPUT_PULLUP);
+  Serial.begin(115200);
 }
 
 // the loop function runs over and over again forever
 void loop()
 {
-  digitalWrite(PC13, HIGH); // turn the LED on (HIGH is the voltage level)
-  delay(1000);              // wait for a second
-  digitalWrite(PC13, LOW);  // turn the LED off by making the voltage LOW
-  delay(1000);
-  Serial.println("hello"); // wait for a second
+  buttonState = digitalRead(key);
+  if (buttonState == HIGH)
+    ledFade = 0;
+  else
+  {
+    if (millis() - lastFadeTime > fadeTime)
+    {
+      if (fadeup)
+      {
+        analogWrite(LEDEksternalPin, ledFade);
+        ledFade += dimSpeed;
+        if (ledFade >= 255)
+        {
+          ledFade = 255;
+          fadeup = false;
+        }
+      }
+      else
+      {
+        analogWrite(LEDEksternalPin, ledFade);
+        ledFade -= dimSpeed;
+        if (ledFade <= 0)
+        {
+          ledFade = 0;
+          fadeup = true;
+        }
+      }
+      lastFadeTime = millis();
+    }
+  }
+
+  potValue = analogRead(potPin);
+  vPot = (double)(potValue / 1023.00) * 5.00;
+  Serial.print("Tegangan Analog: ");
+  Serial.print(vPot, 2);
+  Serial.println(" volt");
+  analogWrite(LEDEksternalPin, ledFade);
+  if (vPot < 2)
+    digitalWrite(LEDBuiltIn, HIGH);
+  else
+    digitalWrite(LEDBuiltIn, LOW);
 }
